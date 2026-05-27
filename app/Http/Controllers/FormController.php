b996d3a84c50c6375a7e75f6a50e9767cd754ac2
JudyRose-Moises <judy.rose.moises@adamson.edu.ph>
2026-05-27 13:37:16 +0800

    may 27

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -330,19 +330,19 @@ public function index()
     if (in_array(strtolower($user->role), ['admin', 'monitor', 'finance'])) {
         // High-level roles see everything
         $workPlans = \App\Models\WorkPlan::all();
-    } elseif (strtoupper($user->role) === 'APPROVER' && strtoupper($user->operating_department) === 'OGM') {
+    } elseif (strtoupper($user->role) === 'DEPARTMENT MANAGER' && strtoupper($user->operating_department) === 'OGM') {
         $targetRCs = ['OGM', 'OAGM', 'SMO', 'PIU', 'IAD', 'LAD', 'PPIMD'];
         $workPlans = \App\Models\WorkPlan::whereIn('r_center', $targetRCs)->get();
     
-    } elseif (strtoupper($user->role) === 'APPROVER' && strtoupper($user->operating_department) === 'ERD') {
+    } elseif (strtoupper($user->role) === 'DEPARTMENT MANAGER' && strtoupper($user->operating_department) === 'ERD') {
         $targetRCs = ['CPD', 'ED', 'SMD', 'ECO'];
         $workPlans = \App\Models\WorkPlan::whereIn('r_center', $targetRCs)->get();
     } 
-     elseif (strtoupper($user->role) === 'APPROVER' && strtoupper($user->operating_department) === 'RMDD') {
+     elseif (strtoupper($user->role) === 'DEPARTMENT MANAGER' && strtoupper($user->operating_department) === 'RMDD') {
         $targetRCs = ['PDMED', 'CDD', 'ELRD'];
         $workPlans = \App\Models\WorkPlan::whereIn('r_center', $targetRCs)->get();
     } 
-     elseif (strtoupper($user->role) === 'APPROVER' && strtoupper($user->operating_department) === 'MSD') {
+     elseif (strtoupper($user->role) === 'DEPARTMENT MANAGER' && strtoupper($user->operating_department) === 'MSD') {
         $targetRCs = ['ADMIN', 'FINANCE'];
         $workPlans = \App\Models\WorkPlan::whereIn('r_center', $targetRCs)->get();
     } 
